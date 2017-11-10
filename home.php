4fa89d3198b77fcfef46f6bbb131fe48126d7993
OrnellaRobs <orazafin@student.42.fr>
2017-11-10 18:35:38 +0100

    update

--- home.php
+++ home.php
@@ -19,4 +19,73 @@ if (!empty($_POST))
 ?>
 <?php require 'inc/header.php'; ?>
 <h1>Bonjour <?= $_SESSION['auth']->name; ?></h1>
-<?php require 'inc/footer.php'; ?>
+<video id="video"></video>
+<button id="startbutton">Prendre une photo</button>
+<canvas id="canvas"></canvas>
+<!-- <img src="http://placekitten.com/g/320/261" id="photo" alt="photo"> -->
+<label for="">
+	<INPUT type="radio" name="choix" value="1">donut.png
+	<INPUT type="radio" name="choix" value="2">best-choice.png
+	<INPUT type="radio" name="choix" value="3">pow.png
+</label>
+<script>
+(function() {
+	var streaming = false,
+	video = document.querySelector('#video'),
+	cover        = document.querySelector('#cover'),
+	canvas       = document.querySelector('#canvas'),
+	photo        = document.querySelector('#photo'),
+	startbutton  = document.querySelector('#startbutton'),
+	width = 520,
+	height = 0;
+
+	navigator.getMedia = ( navigator.getUserMedia ||
+		navigator.webkitGetUserMedia ||
+		navigator.mozGetUserMedia ||
+		navigator.msGetUserMedia);
+		navigator.getMedia(
+			{
+				video: true,
+				audio: false
+			},
+			function(stream) {
+				if (navigator.mozGetUserMedia) {
+					video.mozSrcObject = stream;
+				} else {
+					var vendorURL = window.URL || window.webkitURL;
+					video.src = vendorURL.createObjectURL(stream);
+				}
+				video.play();
+			},
+			function(err) {
+				console.log("An error occured! " + err);
+			}
+		);
+		video.addEventListener('canplay', function(ev){
+			if (!streaming) {
+				height = video.videoHeight / (video.videoWidth/width);
+				video.setAttribute('width', width);
+				video.setAttribute('height', height);
+				canvas.setAttribute('width', width);
+				canvas.setAttribute('height', height);
+				streaming = true;
+			}
+		}, false);
+		startbutton.addEventListener('click', function(ev){
+			takepicture();
+			ev.preventDefault();
+		}, false);
+		function takepicture() {
+			canvas.width = width;
+			canvas.height = height;
+			canvas.getContext('2d').drawImage(video, 0, 0, width, height);
+			var data = canvas.toDataURL('image/png');
+			photo.setAttribute('src', data);
+		}
+		startbutton.addEventListener('click', function(ev){
+			takepicture();
+			ev.preventDefault();
+		}, false);
+	})();
+	</script>
+	<?php require 'inc/footer.php'; ?>

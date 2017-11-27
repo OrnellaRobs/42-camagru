b29dcad4ba7fea7f113381a936e47f515ce74422
OrnellaRobs <orazafin@student.42.fr>
2017-11-27 19:28:30 +0100

    display-gallery-choose-filter-success

--- gallery.php
+++ gallery.php
@@ -4,29 +4,47 @@ check_session();
 logged_only();
 
 require 'inc/header.php';
+require './inc/db.php';
+$user_id = $_SESSION['auth']->id;
+$req = $pdo->prepare('SELECT photo_id FROM likephoto WHERE userlike_id = :userlikeid');
+$req->execute(['userlikeid' => $user_id]);
+$allphotoliked = $req->fetchAll(PDO::FETCH_COLUMN, 0);
 ?>
 <h1>Afficher selon les filtres :</h1>
-<a href="gallery.php?filter=donut"><img src="./images/DONUT.png" width="100px"></a>
-<a href="gallery.php?filter=pizza"><img src="./images/pizza.png" width="100px"></a>
-<a href="gallery.php?filter=pow"><img src="./images/POW.png" width="100px"></a>
+<a href="gallery.php?filter=1"><img src="./images/DONUT.png" width="100px"></a>
+<a href="gallery.php?filter=2"><img src="./images/pizza.png" width="100px"></a>
+<a href="gallery.php?filter=3"><img src="./images/POW.png" width="100px"></a><br/>
 <?php if (!empty($_GET))
 {
-	
+	$request = $pdo->prepare('SELECT * FROM photos WHERE filter = :filter');
+	$request->execute(['filter' => $_GET['filter']]);
+	$allPhotoWithFilter = $request->fetchAll();
+	foreach($allPhotoWithFilter as $filter) {
+		echo '<div class="">';
+	 	echo '<img src="'.$filter->photo_path.'" height="200px" />';
+		$liked = false;
+		foreach ($allphotoliked as $photo) {
+			if ($photo === $filter->photo_id) {
+				$liked = true;
+				break;
+			}
+		}
+		if ($liked == true)
+			echo "<img class='liked' src='images/heart-3.png' width='23px' onClick='toggle(this,\"$filter->photo_id\");'>";
+		else
+			echo "<img class='unliked' src='images/heart-4.png' width='23px' onClick='toggle(this,\"$filter->photo_id\");'>";
+		echo "<a href='comment.php?url=$filter->photo_path&photoid=$filter->photo_id'><img src='images/comment.png' width='40px'></a>";
+		echo '</div>';
+	}
 }
 ?>
 <?php if (empty($_GET)):?>
 <h1>Les photos des autres utilisateurs</h1>
 <div class="wrapper-user-photo">
 	<?php
-	require './inc/db.php';
 	$req = $pdo->prepare('SELECT * FROM photos ORDER BY date_photo');
 	$req->execute();
 	$result = $req->fetchAll();
-
-	$user_id = $_SESSION['auth']->id;
-	$req = $pdo->prepare('SELECT photo_id FROM likephoto WHERE userlike_id = :userlikeid');
-	$req->execute(['userlikeid' => $user_id]);
-	$allphotoliked = $req->fetchAll(PDO::FETCH_COLUMN, 0);
 
 	foreach ($result as $elem)
 	{

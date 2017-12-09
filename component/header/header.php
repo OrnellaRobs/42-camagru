fc904b5d1dafbc920c9b752e9fccb019ca6d0606
OrnellaRobs <orazafin@student.42.fr>
2017-12-09 22:19:07 +0100

    update

--- component/header/header.php
+++ component/header/header.php
@@ -3,10 +3,10 @@
 <head>
 	<meta charset="utf-8">
 	<title>CAMAGRU</title>
-	<link rel="stylesheet" type="text/css" href="/css/main.css">
-	<link rel="stylesheet" type="text/css" href="/css/home.css">
-	<link rel="stylesheet" type="text/css" href="/css/navbar.css">
-	<link rel="stylesheet" type="text/css" href="/css/login-page.css">
-	<link rel="stylesheet" type="text/css" href="/css/footer.css">
+	<link rel="stylesheet" type="text/css" href="/Camagru-Grafik-Art/css/main.css">
+	<link rel="stylesheet" type="text/css" href="/Camagru-Grafik-Art/css/home.css">
+	<link rel="stylesheet" type="text/css" href="/Camagru-Grafik-Art/css/navbar.css">
+	<link rel="stylesheet" type="text/css" href="/Camagru-Grafik-Art/css/login-page.css">
+	<link rel="stylesheet" type="text/css" href="/Camagru-Grafik-Art/css/footer.css">
 </head>
 <body>
